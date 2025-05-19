cd733e8459dad60d4a859ddedc6cd60b076fbdbc
boradogan <doan.bora@gmail.com>
2025-05-19 22:58:08 +0300

    completed: 11_tempConversion

--- 11_tempConversion/tempConversion.js
+++ 11_tempConversion/tempConversion.js
@@ -1,8 +1,16 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(fahrenheit) {
+  return roundDecimal((fahrenheit - 32) * 5 / 9);
+  
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(celcius) {
+  return roundDecimal(celcius * 9 / 5 + 32);
+  
 };
+
+function roundDecimal(number) {
+  return Math.round(number * 10) / 10;
+}
 
 // Do not edit below this line
 module.exports = {
