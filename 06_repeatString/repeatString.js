1b62c21b548a9254125d9d0febee0676a6ee6aad
boradogan <doan.bora@gmail.com>
2025-05-19 22:15:40 +0300

    completed: 06_repeatString

--- 06_repeatString/repeatString.js
+++ 06_repeatString/repeatString.js
@@ -1,4 +1,13 @@
-const repeatString = function() {
+const repeatString = function(str, repeatingTimes) {
+
+    if (repeatingTimes < 0) {
+        return "ERROR"
+    }
+    let accumulator = '';
+    for (let i = 0; i < repeatingTimes; i++){
+        accumulator = accumulator + str;
+    }
+    return accumulator;
 
 };
 
