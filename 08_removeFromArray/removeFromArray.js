cb3709bde5e525dfdae5c2a8ede9f6a22a6d7ed8
boradogan <doan.bora@gmail.com>
2025-05-19 22:39:29 +0300

    complete: 08_removeFromArray

--- 08_removeFromArray/removeFromArray.js
+++ 08_removeFromArray/removeFromArray.js
@@ -1,4 +1,7 @@
-const removeFromArray = function() {
+const removeFromArray = function(arr, ...numbers) {
+    return arr.filter(item => !numbers.includes(item));
+    
+
 };
 
 // Do not edit below this line
