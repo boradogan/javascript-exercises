df6f2df0f2dc8d5016710ee36f130915f95a513d
boradogan <doan.bora@gmail.com>
2025-05-21 19:48:56 +0300

    completed : 14_fibonacci

--- 14_fibonacci/fibonacci.js
+++ 14_fibonacci/fibonacci.js
@@ -1,4 +1,15 @@
-const fibonacci = function() {
+const fibonacci = function(index) {
+    fib_seq = [1, 1];
+    index = parseInt(index, 10);
+    if (index === 0) {
+        return 0
+    } else if (index < 0) {
+        return 'OOPS';
+    }
+    while (fib_seq.length < index) {
+        fib_seq.push(fib_seq.at(-1) + fib_seq.at(-2));
+    }
+    return fib_seq.at(-1);
 
 };
 
