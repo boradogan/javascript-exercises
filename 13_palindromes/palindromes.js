5eb340565ccf87a9b19de3250f70573a07ffc7a9
boradogan <doan.bora@gmail.com>
2025-05-21 19:43:20 +0300

    completed: 13_palindromes

--- 13_palindromes/palindromes.js
+++ 13_palindromes/palindromes.js
@@ -1,4 +1,15 @@
-const palindromes = function () {
+const palindromes = function (word) {
+    word = word.toLowerCase().replace(/[^a-z0-9]/g, '');
+    indexStart = 0;
+    indexEnd = word.length - 1;
+    while (indexEnd > indexStart) {
+        if (word[indexStart] !== word[indexEnd]){
+            return false
+        }
+        indexStart++;
+        indexEnd--;
+    }
+    return true
 
 };
 
