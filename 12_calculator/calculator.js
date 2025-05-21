fb789887c7d28fe0a354e03fbcec235076889885
boradogan <doan.bora@gmail.com>
2025-05-21 19:37:35 +0300

    completed: 12_calculator

--- 12_calculator/calculator.js
+++ 12_calculator/calculator.js
@@ -1,24 +1,34 @@
-const add = function() {
+const add = function(a , b) {
+  return a + b;
 	
 };
 
-const subtract = function() {
+const subtract = function(a , b) {
+  return a - b;
 	
 };
 
-const sum = function() {
+const sum = function(arr) {
+  return arr.reduce((total, num) => total + num, 0);
 	
 };
 
-const multiply = function() {
+const multiply = function(arr) {
+  return arr.reduce((total, num) => total * num, 1);
 
 };
 
-const power = function() {
+const power = function(a, b) {
+  return a ** b;
 	
 };
 
-const factorial = function() {
+const factorial = function(num) {
+  let mult = 1;
+  for (let i = 1; i <= num; i++) {
+    mult *= i;
+  }
+  return mult;
 	
 };
 
